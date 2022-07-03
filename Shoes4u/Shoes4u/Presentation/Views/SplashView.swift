b946520fb3435e055b7b2fcd7d659c4763d33dcd
Mikel Cobian <mikelcobian@gmail.com>
2022-07-03 13:01:32 +0200

    limpieza de cambios

---
+++ Shoes4u/Shoes4u/Presentation/Views/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  Shoes4u
+//
+//  Created by Mikel Cobian on 28/6/22.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
