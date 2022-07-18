a954e307e3fc2fe3e7e4aba8e2506e0615e5819c
Mikel Cobian <mikelcobian@gmail.com>
2022-07-18 15:10:48 +0200

    subida para jose luis 2

--- Shoes4u/Shoes4u/Shoes4uApp.swift
+++ Shoes4u/Shoes4u/Shoes4uApp.swift
@@ -12,15 +12,7 @@ import Firebase
 struct Shoes4uApp: App {
     var body: some Scene {
         WindowGroup {
-            ProductsView(products: [Product(
-                id: UUID(),
-                category:"Baloncesto",
-                description:"Zapatillas que sirven para jugar al baloncesto",
-                imagenUrl:
-                "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.lavanguardia.com%2Ffiles%2Fcontent_image_mobile_filter%2Fuploads%2F2021%2F09%2F02%2F61309fe9cc88d.png&imgrefurl=https%3A%2F%2Fwww.lavanguardia.com%2Fcomprar%2Fmoda-belleza%2F20210903%2F7694958%2Fentrenamientos-zapatillas-deportivas-marca-affi-a2s.html&tbnid=_vZCqCA5NN-puM&vet=12ahUKEwi04Y7n8Pr4AhX1gc4BHRz4DjoQMygLegUIARDgAw..i&docid=RGKFi53BvfXg3M&w=449&h=362&q=zapatillas%20deportivas&client=safari&ved=2ahUKEwi04Y7n8Pr4AhX1gc4BHRz4DjoQMygLegUIARDgAw",
-                name:"Zapatillas de baloncesto",
-                price:
-                40)])
+            ProductsView()
         }
     }
 }
